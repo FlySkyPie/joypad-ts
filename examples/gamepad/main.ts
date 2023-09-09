78f4c84decebebe2c988bceb5af9ecf54904c2fa
Wei Ji(FlySkyPie) <c445dj544@gmail.com>
2023-09-09 16:51:36 +0800

    fix: Add a example

---
+++ examples/gamepad/main.ts
@@ -0,0 +1,68 @@
+import joypad from "@joypad-ts";
+
+import "./sticks";
+
+const gamepadSVG = document.querySelector<SVGSVGElement>("#gampad")!;
+
+function reportWindowSize() {
+  // heightOutput.textContent = window.innerHeight.toString();
+  // widthOutput.textContent = window.innerWidth.toString();
+  gamepadSVG.setAttribute("width", (window.innerWidth * 0.5).toString());
+  gamepadSVG.setAttribute("height", window.innerHeight.toString());
+}
+addEventListener("resize", reportWindowSize);
+reportWindowSize();
+
+const buttonElements = [
+  document.querySelector<SVGSVGElement>(".B10")!, // LeftStick
+  document.querySelector<SVGSVGElement>(".B11")!, // RightStick
+  document.querySelector<SVGSVGElement>(".R1")!,
+  document.querySelector<SVGSVGElement>(".R2")!,
+
+  // RightButtons
+  document.querySelector<SVGSVGElement>(".B03")!,
+  document.querySelector<SVGSVGElement>(".B02")!,
+  document.querySelector<SVGSVGElement>(".B01")!,
+  document.querySelector<SVGSVGElement>(".B00")!,
+
+  // LeftButtons
+  document.querySelector<SVGSVGElement>(".B12")!,
+  document.querySelector<SVGSVGElement>(".B14")!,
+  document.querySelector<SVGSVGElement>(".B15")!,
+  document.querySelector<SVGSVGElement>(".B13")!,
+
+  // CenterButtons
+  document.querySelector<SVGSVGElement>(".B08")!,
+  document.querySelector<SVGSVGElement>(".B09")!,
+  document.querySelector<SVGSVGElement>(".R2")!,
+  document.querySelector<SVGSVGElement>(".L2")!,
+];
+
+let heading = document.getElementById("heading")!;
+let message = document.getElementById("message")!;
+
+function resetInfo() {
+  heading.innerText = "No controller connected!";
+  message.innerText = "Please connect a controller and press any key to start.";
+}
+
+function updateInfo(e: GamepadEvent) {
+  const { gamepad } = e;
+
+  heading.innerText = "Controller connected!";
+  message.innerText = gamepad.id;
+}
+
+resetInfo();
+joypad.on("connect", (e) => {
+  console.log(e);
+  return updateInfo(e);
+});
+joypad.on("disconnect", (e) => {
+  console.log(e);
+  return resetInfo();
+});
+
+joypad.set({
+  axisMovementThreshold: 0.3,
+});
---
+++ examples/gamepad/sticks.ts
@@ -0,0 +1,92 @@
+import joypad from "@joypad-ts";
+
+type IAxisControls = {
+  left: { x: number; y: number; timer?: NodeJS.Timeout };
+  right: { x: number; y: number; timer?: NodeJS.Timeout };
+};
+
+const axisControls: IAxisControls = {
+  left: { x: 0, y: 0 },
+  right: { x: 0, y: 0 },
+};
+
+const leftStickElement = document.querySelector<SVGSVGElement>(".B10")!; // LeftStick
+const rightStickElement = document.querySelector<SVGSVGElement>(".B11")!; // RightStick
+
+const updateLeftStick = () => {
+  leftStickElement.style.transform = `translate(${axisControls.left.x * 32}px,${
+    axisControls.left.y * 32
+  }px)`;
+
+  if (
+    Math.abs(axisControls.left.x) > 0.1 ||
+    Math.abs(axisControls.left.y) > 0.1
+  ) {
+    leftStickElement.style.fill = "#990000";
+  } else {
+    leftStickElement.style.fill = "#000000";
+  }
+};
+
+const updateRightStick = () => {
+  rightStickElement.style.transform = `translate(${
+    axisControls.right.x * 32
+  }px,${axisControls.right.y * 32}px)`;
+
+  if (
+    Math.abs(axisControls.right.x) > 0.1 ||
+    Math.abs(axisControls.right.y) > 0.1
+  ) {
+    rightStickElement.style.fill = "#990000";
+  } else {
+    rightStickElement.style.fill = "#000000";
+  }
+};
+
+joypad.on("axis_move", ({ detail }) => {
+  const { directionOfMovement, stickMoved, axisMovementValue } = detail;
+  console.log("detail", detail);
+
+  if (stickMoved === "left_stick") {
+    switch (directionOfMovement) {
+      case "left":
+      case "right":
+        axisControls.left.x = axisMovementValue;
+        break;
+      case "top":
+      case "bottom":
+        axisControls.left.y = axisMovementValue;
+        break;
+    }
+
+    axisControls.left.timer && clearTimeout(axisControls.left.timer);
+    axisControls.left.timer = setTimeout(() => {
+      axisControls.left.x = 0;
+      axisControls.left.y = 0;
+      updateLeftStick();
+    }, 500);
+  }
+
+  if (stickMoved === "right_stick") {
+    switch (directionOfMovement) {
+      case "left":
+      case "right":
+        axisControls.right.x = axisMovementValue;
+        break;
+      case "top":
+      case "bottom":
+        axisControls.right.y = axisMovementValue;
+        break;
+    }
+
+    axisControls.right.timer && clearTimeout(axisControls.right.timer);
+    axisControls.right.timer = setTimeout(() => {
+      axisControls.right.x = 0;
+      axisControls.right.y = 0;
+      updateRightStick();
+    }, 500);
+  }
+
+  updateLeftStick();
+  updateRightStick();
+});
